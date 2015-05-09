059407353626cee3aa98536f834a0965e0f3d1e4
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-05-09 16:18:46 +0900

    Create Question1 projects

---
+++ question1/AutoLayout-question1/AutoLayout-question1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout-question1
+//
+//  Created by shinbo maiki on 2015/05/09.
+//  Copyright (c) 2015年 shinbo maiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
