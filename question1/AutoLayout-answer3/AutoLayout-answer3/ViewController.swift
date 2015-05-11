bc145c9383d265baae8ec3d36c8f09cba1533456
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-05-11 11:28:56 +0900

    Create Answer3 projects & Setting Answer3 layout

---
+++ question1/AutoLayout-answer3/AutoLayout-answer3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout-answer3
+//
+//  Created by shinbo maiki on 2015/05/11.
+//  Copyright (c) 2015年 shinbo maiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
